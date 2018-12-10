68ed43a994fd5aaa16caaf979324e4c39ca67aa5
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-09 19:48:58 -0500

    Fixed first block (added genesis block) and changed switch statement

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,7 @@ genesis_block = {
     'index': 0,
     'transactions': []
 }
-blockchain = []
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Fred'
 
@@ -38,6 +38,11 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 def mine_block():
     last_block = blockchain[-1]
+    hashed_block = ''
+    for keys in last_block:
+        value = last_block[keys]
+        hashed_block += str(value)
+    print(hashed_block)
     block = {
         'previous_hash': 'XYZ',
         'index': len(blockchain),
@@ -87,7 +92,8 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -98,6 +104,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         # Make sure that you don't try to "hack" the blockchain if it's empty
