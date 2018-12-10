2b5282f6e7850a7667d8e730f54a0697721a8b26
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-09 19:44:39 -0500

    Creating the blockchain functionality

---
+++ blockchain.py
@@ -0,0 +1,118 @@
+# Initializing our blockchain list
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = []
+open_transactions = []
+owner = 'Fred'
+
+
+def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
+    if len(blockchain) < 1:
+        return None
+    return blockchain[-1]
+
+# This function accepts two arguments.
+# One required one (transaction_amount) & one optional one(last_transaction)
+# The optional one is optional because it has a default value => [1]
+
+
+def add_transaction(recipient, sender=owner, amount=1.0):
+    """ Append a new value as well as the last blockchain value to the blockchain.
+
+    Arguments:
+            :sender: The sender of the coins.
+            :recipient: The recipient of the coins.
+            :amount: The amount of coins send with the transaction (default = 1.0)
+    """
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    }
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    last_block = blockchain[-1]
+    block = {
+        'previous_hash': 'XYZ',
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
+
+
+def get_transaction_value():
+    """ Returns the input of the user (a new transaction amount) as a float. """
+    # Get the user input, transform it from a string to a float and store it
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    return (tx_recipient, tx_amount)
+
+
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+
+def print_blockchain_elements():
+    # Output the blockchain list to the console
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+    else:
+        print('-' * 20)
+
+
+def verify_chain():
+    # block_index = 0
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    return is_valid
+
+
+waiting_for_input = True
+
+while waiting_for_input:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        # Add the transaction amount to the blockchain
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'h':
+        # Make sure that you don't try to "hack" the blockchain if it's empty
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == 'q':
+        # This will lead to the loop to exit because it's running condition
+        waiting_for_input = False
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain')
+        break
+else:
+    print('User left!')
+
+print('Done!')
---
+++ main.py
@@ -0,0 +1,38 @@
+# name = input('What is your name? ')
+# age = float(input('How old are you? '))
+
+
+# def print_text():
+#     print(name + ' is ' + str(age))
+
+
+# print_text()
+
+
+# def print_sentence(first, second):
+#     print(first + ' ' + second)
+
+
+# print_sentence('Hello', 'World')
+
+
+# def calc_decade(age):
+#     decades = age // 10
+#     return print('You have lived for ' + str(decades) + ' decades')
+
+
+# calc_decade(24)
+
+# names = ['Fred', 'freddyal', 'sean', 'andreas', 'sirfred']
+
+# for name in names:
+# 	print(name)
+# 	print(name + ' has ' + str(len(name)) + ' characters')
+# 	if len(name) > 5:
+# 		print(name + ' is longer than 5 characters')
+# 	if ('n' or 'N') in name:
+# 		print(name + ' has an N or n in the name')
+
+# while len(names) >= 1:
+# 	names.pop()
+# 	print(names)
