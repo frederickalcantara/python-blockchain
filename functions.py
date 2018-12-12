6428d4daed12ba7d961542c1c2f31a205a67f2e0
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-11 21:37:45 -0500

    Simplified balance calculation

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+"""
 def unlimited_arguments(*args):
     print(args)
     for argument in args:
@@ -5,12 +6,13 @@ def unlimited_arguments(*args):
 
 
 unlimited_arguments(*[1, 2, 3, 4])
+"""
 
 # Unpacking function arguments
 """
 You can use the * symbol in a parameter to turn a group of arguments into a tuple
-It's useful for unpacking arguments, works for lists & tuples
+It's useful for unpacking arguments to single value, works for lists & tuples
 
 You can also use the ** in a paramter to turn loop through the keys of dictionary.
-It's useful for unpacking many arguments for if you use dictionaries as an arguments
+It's useful for unpacking many arguments to single value, if you use dictionaries as an arguments
 """
