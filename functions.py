0d738bda4768922c10cce5216c9f7e03223e680d
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-11 22:22:57 -0500

    Created coding work to practice python function conventions

--- functions.py
+++ functions.py
@@ -5,8 +5,9 @@ def unlimited_arguments(*args):
         print(argument)
 
 
-unlimited_arguments(*[1, 2, 3, 4])
+print(unlimited_arguments(*[1, 2, 3, 4]))
 """
+
 
 # Unpacking function arguments
 """
@@ -16,3 +17,37 @@ It's useful for unpacking arguments to single value, works for lists & tuples
 You can also use the ** in a paramter to turn loop through the keys of dictionary.
 It's useful for unpacking many arguments to single value, if you use dictionaries as an arguments
 """
+
+# 1. Write a function that accepts another function as an argument
+"""
+simple_list = [1, 2, 3, 4]
+
+def multiply(el):
+    return el * 4
+
+print(list(map(multiply, simple_list)))
+"""
+
+# 2. Write a function like #1 but with a lambda function
+"""
+simple_list = [1, 2, 3, 4]
+
+print(list(map(lambda el: el * 4, simple_list)))
+"""
+
+# 3. Write a function that allows for infinite arguments
+
+"""
+def multiply4(fn, *args):
+    for arg in args:
+        print(fn(arg))
+
+
+print(multiply4(lambda x: x * 4, 1, 2, 3, 4, 5))
+"""
+
+# 4. Format output to have numbers look nice and are centered in a 20 character column
+
+simple_list = [1, 2, 3, 4]
+
+print('Number format: {:^20}'.format(str(simple_list)))
