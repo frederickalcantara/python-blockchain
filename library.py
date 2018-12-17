17349073b79366d7adbaee5a3e11d550083c43fb
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-16 21:08:19 -0500

    Solved library assignment

--- library.py
+++ library.py
@@ -1,2 +1,11 @@
-import random
-import datetime
+from random import random, uniform
+from datetime import time
+
+rn1 = random()
+rn2 = uniform(1, 10)
+
+print(rn1, rn2)
+
+hour = time(int(uniform(0, 24)))
+
+print(hour)
