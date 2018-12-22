349d50939a45aaca715520de9121e85313e3cfcd
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-22 18:47:10 -0500

    Added getters & setters for the blockchain

--- node.py
+++ node.py
@@ -52,15 +52,14 @@ class Node:
                     print("Added transaction!")
                 else:
                     print("Transaction failed!")
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == "2":
                 self.blockchain.mine_block()
             elif user_choice == "3":
                 self.print_blockchain_elements()
             elif user_choice == "4":
-                verifier = Verification()
-                if verifier.verify_transactions(
-                    self.blockchain.open_transactions, self.blockchain.get_balance
+                if Verification.verify_transactions(
+                    self.blockchain.get_open_transactions(), self.blockchain.get_balance
                 ):
                     print("All transactions are valid")
                 else:
@@ -70,8 +69,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print("Input was invalid, please pick a value from the list!")
-            verifier = Verification()
-            if not verifier.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_chain()):
                 self.print_blockchain_elements()
                 print("Invalid blockchain!")
                 # Break out of the loop
