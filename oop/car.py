11b0c00151e7bf69b21b1e2bb45dd89f974d3ae9
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-21 21:33:02 -0500

    Added special methods

--- oop/car.py
+++ oop/car.py
@@ -1,5 +1,13 @@
 class Car():
-    top_speed = 160
+    # top_speed = 160
+    # warnings = []
+    def __init__(self, starting_top_speed=160):
+        self.top_speed = starting_top_speed
+        self.warnings = []
+
+    def __repr__(self):
+        print('Printing...')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, self.warnings)
 
     def drive(self):
         print('I am driving not certainly faster than {}'.format(self.top_speed))
@@ -7,3 +15,18 @@ class Car():
 
 car1 = Car()
 car1.drive()
+
+# Car.top_speed = 200
+car1.warnings.append('New Warning')
+# print(car1.__dict__)
+print(car1)
+
+car2 = Car(200)
+car2.drive()
+
+car3 = Car(250)
+car3.drive()
+
+
+# __init__ is a constructor in python
+# Have to return self in any method for an object
