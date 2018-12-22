3ecc5cfe05e79443ddabc1b84e8d931e9013731b
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-22 15:44:23 -0500

    Added Inheritance printing dictions for refactoring

--- blockchain.py
+++ blockchain.py
@@ -17,8 +17,6 @@ blockchain = []
 open_transactions = []
 # We are the owner of this blockchain node, hence this is our identifier (e.g. for sending coins)
 owner = "Fred"
-# Registered participants: Ourself + other people sending/ receiving coins
-participants = {"Fred"}
 
 
 def load_data():
@@ -286,8 +284,7 @@ while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
-    print("4: Output participants")
-    print("5: Check transaction validity")
+    print("4: Check transaction validity")
     print("q: Quit")
     user_choice = get_user_choice()
     if user_choice == "1":
@@ -306,8 +303,6 @@ while waiting_for_input:
     elif user_choice == "3":
         print_blockchain_elements()
     elif user_choice == "4":
-        print(participants)
-    elif user_choice == "5":
         if verify_transactions():
             print("All transactions are valid")
         else:
