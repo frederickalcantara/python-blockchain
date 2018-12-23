243c8280b570cd178a8a16ca3426866bd1c40e11
frederickalcantara <frederickaalcantara@gmail.com>
2018-12-23 14:36:52 -0500

    Connecting the node with the wallet public & private keys

--- node.py
+++ node.py
@@ -2,13 +2,14 @@ from uuid import uuid4
 
 from blockchain import Blockchain
 from utility.verification import Verification
+from wallet import Wallet
 
 
 class Node:
     def __init__(self):
         # self.id = str(uuid4())
-        self.id = "Fred"
-        self.blockchain = Blockchain(self.id)
+        self.wallet = Wallet()
+        self.blockchain = Blockchain(self.wallet.public_key)
 
     def get_transaction_value(self):
         """ Returns the input of the user (a new transaction amount) as a float. """
@@ -50,7 +51,9 @@ class Node:
                 tx_data = self.get_transaction_value()
                 recipient, amount = tx_data
                 # Add the transaction amount to the blockchain
-                if self.blockchain.add_transaction(recipient, self.id, amount=amount):
+                if self.blockchain.add_transaction(
+                    recipient, self.wallet.public_key, amount=amount
+                ):
                     print("Added transaction!")
                 else:
                     print("Transaction failed!")
@@ -66,6 +69,10 @@ class Node:
                     print("All transactions are valid")
                 else:
                     print("There are invalid transactions")
+            elif user_choice == "5":
+                self.wallet.create_keys()
+            elif user_choice == "6":
+                pass
             elif user_choice == "q":
                 # This will lead to the loop to exist because it's running condition becomes False
                 waiting_for_input = False
@@ -77,7 +84,9 @@ class Node:
                 # Break out of the loop
                 break
             print(
-                "Balance of {}: {:6.2f}".format(self.id, self.blockchain.get_balance())
+                "Balance of {}: {:6.2f}".format(
+                    self.wallet.public_key, self.blockchain.get_balance()
+                )
             )
         else:
             print("User left!")
